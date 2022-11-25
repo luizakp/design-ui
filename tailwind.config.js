cef1c9ab8cf3cbe29258960f51b2cfb734356d4d
luizakp <luiza.puccinelli06@gmail.com>
2022-11-25 15:51:56 +0100

    wip: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,26 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
-const brandColor = colors.blue;
+const neutralColor = {
+	100: '#FFFFFF',
+	200: '#F7F7F9',
+	300: '#EBECF2',
+	400: '#D7DDEA',
+	500: '#BBC3D4',
+	600: '#A5ADBF',
+	700: '#82899A',
+	800: '#4F576A',
+	900: '#4F576A',
+};
 
 module.exports = {
-	// Add support for dark mode, toggled via a class:
-	// https://tailwindcss.com/docs/dark-mode
 	darkMode: 'class',
-	// Inform Tailwind of where our classes will be defined:
-	// https://tailwindcss.com/docs/optimizing-for-production
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 
 	theme: {
 		extend: {
 			colors: {
-				// NOTE: We modify the gray color, as the default Tailwind gray color is heavily saturated
-				// with blue, which makes it look strange in dark mode. This gray color is more balanced,
-				// and works better for sites supporting dark mode.
-				gray: colors.gray,
-				// Add a new "brand" color to all Tailwind utilities, so that we can easily change it.
-				brand: brandColor,
-			},
-			// Modify the default ring color so that it matches the brand color:
-			ringColor: {
-				DEFAULT: brandColor['500'],
+				neutral: neutralColor,
 			},
 		},
 	},
