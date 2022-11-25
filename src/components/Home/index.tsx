413ba4944a217398f66bda344447625c55a819f9
luizakp <luiza.puccinelli06@gmail.com>
2022-11-25 14:30:21 +0100

    test deploy

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,19 +5,7 @@ export function Home() {
 	return (
 		<Container>
 			<div className="space-y-6">
-				<div className="text-8xl font-bold italic text-center">
-					<span className="bg-clip-text text-transparent bg-gradient-to-r from-[#9b51e0] to-[#2d9cdb]">
-						nytro
-					</span>
-				</div>
-				<div className="mt-8 prose-lg">
-					Welcome to . This project contains a very minimal note-taking
-					application.
-				</div>
-
-				<Button href="/notes" fullWidth>
-					View Your Notes
-				</Button>
+				<Button fullWidth>Test</Button>
 			</div>
 		</Container>
 	);
